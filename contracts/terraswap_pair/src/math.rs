5522cf1fc83a980e6410105f9e6de26ad3897c21
Eugene <we.danced.a.lot@gmail.com>
2021-11-04 20:29:09 +0200

    Use Decimal256 to prevent overflow

--- contracts/terraswap_pair/src/math.rs
+++ contracts/terraswap_pair/src/math.rs
@@ -1,7 +1,9 @@
+use cosmwasm_bignumber::{Decimal256, Uint256};
 use cosmwasm_std::Uint128;
 use fixed::transcendental::pow as fixed_pow;
 use fixed::types::I64F64;
 use std::ops::{Add, Div, Mul, Sub};
+use std::str::FromStr;
 
 pub type FixedFloat = I64F64;
 
@@ -16,9 +18,13 @@ pub fn calc_out_given_in(
         return Uint128::zero();
     }
 
-    let adjusted_in = balance_in.add(amount_in);
+    // Use 256 so that to prevent overflow error
+    let balance_in: Uint256 = balance_in.into();
+    let adjusted_in = balance_in.add(amount_in.into());
 
-    let y = FixedFloat::from_num(balance_in.u128()).div(&FixedFloat::from_num(adjusted_in.u128()));
+    let y256 = Decimal256::from_ratio(balance_in, adjusted_in).to_string();
+    let y = FixedFloat::from_str(&y256).unwrap();
+
     let weight_ratio = weight_in.div(&weight_out);
 
     let multiplier: FixedFloat = fixed_pow(y, weight_ratio).unwrap();
@@ -38,12 +44,14 @@ pub fn calc_in_given_out(
     weight_out: FixedFloat,
     amount_out: Uint128,
 ) -> Uint128 {
-    let updated_balance = balance_out.checked_sub(amount_out).unwrap();
+    // Use 256 so that to prevent overflow error
+    let balance_out: Uint256 = balance_out.into();
+    let updated_balance = balance_out.sub(amount_out.into());
 
     let weight_ratio = weight_out.div(&weight_in);
 
-    let y =
-        FixedFloat::from_num(balance_out.u128()).div(&FixedFloat::from_num(updated_balance.u128()));
+    let y256 = Decimal256::from_ratio(balance_out, updated_balance).to_string();
+    let y = FixedFloat::from_str(&y256).unwrap();
 
     let multiplier: FixedFloat = fixed_pow(y, weight_ratio).unwrap();
     let multiplier = multiplier.sub(FixedFloat::from_num(1));
